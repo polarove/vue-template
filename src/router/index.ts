12d393fdfdda865793621eb3ddb8635fd45ad4e8
liuqi <liuqi6602@163.com>
2024-02-26 22:39:53 +0800

    refactor: remove auto imported type files

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,32 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 import nProgress from 'nprogress'
 
 const routes = [
-    {
-        path: '/',
-        name: 'index',
-        component: () => import('@pages/index.vue'),
-    },
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('@pages/index.vue')
+  }
 ]
 
-const createRoutes = () => {
-    if (import.meta.env.PROD) {
-        return routes.map((route) => {
-            route.path = `/${import.meta.env.VITE_APP_NAME}${route.path}`
-        })
-    }
-}
-createRoutes()
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes
 })
 
 router.beforeEach((_) => {
-    nProgress.start()
+  nProgress.start()
 })
 
 router.afterEach(() => {
-    nProgress.done()
+  nProgress.done()
 })
 
 export default router
