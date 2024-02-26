0b84af99fa538c300f47f373b62629afa56a13ee
liuqi <liuqi6602@163.com>
2024-02-26 22:31:06 +0800

    update

--- src/util/ListUtil.ts
+++
@@ -1,8 +0,0 @@
-export class ListUtil {
-  static isEmpty(list: any[]) {
-    return isNullOrUndefined(list) || list.length === 0
-  }
-  static isNotEmpty(list: any[]) {
-    return !this.isEmpty(list)
-  }
-}
--- src/util/Util.ts
+++
@@ -1,7 +0,0 @@
-export const isNullOrUndefined = (s: any): boolean => {
-  return s == null || s == undefined
-}
-
-export const isNotNullOrUndefined = (s: any): boolean => {
-  return !isNullOrUndefined(s)
-}
