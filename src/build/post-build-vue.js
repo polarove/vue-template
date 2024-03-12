812826ddc7f5f8730b6c1b39705a7a4876770493
liuqi <liuqi6602@163.com>
2024-03-12 15:43:03 +0800

    feat: asf

--- src/build/post-build-vue.js
+++ src/build/post-build-vue.js
@@ -1,10 +1,17 @@
 import process from 'child_process'
 import { resolve } from 'path'
-const dist = resolve('..\\dist\\')
+import fs from 'fs'
+const dist = resolve('./dist')
+const FILE_NAME = './dist/dist.zip'
+const OUTPUT = './dist/'
 
 ;(function () {
+  if (fs.existsSync(FILE_NAME)) {
+    fs.unlinkSync(FILE_NAME)
+    console.log('previous build has been removed: ' + FILE_NAME)
+  }
   console.log('[部署]：需要提前安装bandizip对dist进行压缩')
-  process.exec('bandizip c -y ../dist/dist.zip ../dist/', (err) => {
+  process.exec(`bandizip c -y ${FILE_NAME} ${OUTPUT}`, (err) => {
     if (!err) {
       process.exec(`explorer ${dist}`)
     } else {
