eb1b572af2a11c4cc15293065e8a59c2b64cf345
liuqi <liuqi6602@163.com>
2024-03-12 14:40:02 +0800

    feat: git pu

--- src/components/usecases/components/card/my-card.vue
+++ src/components/usecases/components/card/my-card.vue
@@ -18,6 +18,24 @@
         fit="cover"
       />
     </rotate-card>
+    <div class="footer">
+      <el-text>
+        Github@<a href="https://github.com/polarove" class="name">polarove</a>
+      </el-text>
+    </div>
+  </section>
+  <section>
+    <div><h2>About me</h2></div>
+    <p>Gender & Sex: male</p>
+    <p>University: Minnan University of Science and Technology (2019 - 2023)</p>
+    <p>Degree: bachelor</p>
+    <p>Major: Infomation Management and Information System (IMIS)</p>
+    <p>
+      Working experience: about&nbsp;{{
+        monthsThatHasWastedInWork()
+      }}&nbsp;months
+    </p>
+    <p>Field: a web fullstack developer</p>
   </section>
 </template>
 
@@ -25,6 +43,31 @@
 const jumpGithub = () => {
   window.open('https://github.com/polarove')
 }
+
+const monthsThatHasWastedInWork = () => {
+  const STARTED_AT = 2023
+  const NOW = new Date()
+  const YEAR = NOW.getFullYear()
+  const MONTH = NOW.getMonth()
+  if (YEAR - STARTED_AT === 1) {
+    return MONTH + 2
+  } else {
+    return (YEAR - 1 - STARTED_AT) * 12 + MONTH
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.footer {
+  height: 3em;
+  text-align: center;
+  .name {
+    &:hover {
+      color: var(--el-color-primary);
+    }
+  }
+  a {
+    color: var(--el-text-color-primary);
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-80% ma-auto">
     <my-header class="mt-4em mb-4em" />
+    <my-card />
     <my-utils />
     <el-divider />
     <my-input />
     <el-divider />
-    <my-card />
     <my-footer />
   </div>
 </template>
